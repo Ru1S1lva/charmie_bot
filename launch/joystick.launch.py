7a71e8bb88d1a56dc6f1f75be2ae38c55e2d9323
Rui Silva <55987669+Ru1S1lva@users.noreply.github.com>
2023-05-29 13:25:40 +0100

    Add files via upload

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -23,6 +23,7 @@ def generate_launch_description():
             executable='teleop_node',
             name='teleop_node',
             parameters=[joy_params],
+            #remappings=[('/cmd_vel', '/cmd_vel_joy')]
          )
 
     return LaunchDescription([
